19b19dfd4202bb946bc5cf28a105a395a5da3667
yairavalosHobOps <yair.avalos@hobops.io>
2023-04-18 21:05:11 -0600

    fix: correcting testings to assure correct error validation

--- tests/base/test_base_domain_exception.py
+++ tests/base/test_base_domain_exception.py
@@ -17,13 +17,13 @@ def test_base_domain_exception_full_validation():
 
     my_exception = BaseDomainException(error_message, error_source, error_type)
 
-    with pytest.raises(BaseDomainException):
+    with pytest.raises(BaseDomainException) as err:
         raise my_exception
 
     assert my_exception.args.__len__() == 1
-    assert my_exception.error_message == error_message
-    assert my_exception.error_source == error_source
-    assert my_exception.error_type == error_type
+    assert my_exception.error_message == err.value.error_message
+    assert my_exception.error_source == err.value.error_source
+    assert my_exception.error_type == err.value.error_type
 
 
 def test_base_domain_exception_minimal_validation():
@@ -35,11 +35,11 @@ def test_base_domain_exception_minimal_validation():
 
     my_exception = BaseDomainException(error_message)
 
-    with pytest.raises(BaseDomainException):
+    with pytest.raises(BaseDomainException) as err:
         raise my_exception
 
     assert my_exception.args.__len__() == 1
-    assert my_exception.error_message == error_message
+    assert my_exception.error_message == err.value.error_message
     assert my_exception.error_source is None
     assert my_exception.error_type is None
 
@@ -49,8 +49,10 @@ def test_base_domain_exception_minimal_validation_fails():
     test_base_domain_exception_minimal_validation_fails
     """
 
-    with pytest.raises(Exception):
+    expected_error_message = "__init__() missing 1 required positional argument: 'error_message'"
+
+    with pytest.raises(Exception) as err:
         my_exception = BaseDomainException()
 
-    assert True
+    assert err.value.args[0] == expected_error_message
 
