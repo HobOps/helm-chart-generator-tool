7e8065a5e60cdce41a1a2f1f46dad485ffa1370a
Yair Avalos HobOps Dev <yair.avalos@hobops.io>
2023-04-20 19:14:05 +0000

    feat: adding test validations on yaml function

--- tests/base/domain/file_management/yaml_file_manager/test_yaml_file_manager.py
+++ tests/base/domain/file_management/yaml_file_manager/test_yaml_file_manager.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import unittest.mock
 
 import yaml
 from unittest.mock import MagicMock
@@ -18,15 +18,25 @@ def test_yaml_file_management_validation():
       namespace: default
       version: 1.0
     """
+
     my_data1 = yaml.unsafe_load(yaml_info)
     my_data2 = yaml.safe_load(yaml_info)
 
-    dumped_data = Mock()
-    yaml.dump(yaml_info, dumped_data)
+    dumped_data1 = Mock()
+    dumped_data2 = Mock()
+
+    yaml.dump(yaml_info, dumped_data1)
+    yaml.dump(my_data2, dumped_data2)
 
     assert my_data1["cluster"]["context"] == "default"
     assert my_data2["cluster"]["context"] == "default"
 
-    assert dumped_data.mock_calls[0][0] == 'write'
-    assert dumped_data.write.mock_calls.__len__() > 0
+    assert dumped_data1.mock_calls[0][0] == 'write'
+    assert dumped_data1.write.mock_calls.__len__() > 0
+    dumped_data2.write.assert_has_calls(calls=[unittest.mock.call("cluster")])
+    dumped_data2.write.assert_has_calls(calls=[unittest.mock.call("context")])
+    dumped_data2.write.assert_has_calls(calls=[unittest.mock.call("namespace")])
+    dumped_data2.write.assert_has_calls(calls=[unittest.mock.call("version")])
     assert True
+
+
