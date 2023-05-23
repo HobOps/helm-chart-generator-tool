23e0bafc201160ab58bb9baa3939ed8640e29914
Yair Avalos HobOps Dev <yair.avalos@hobops.io>
2023-05-23 23:45:38 +0000

    fix: removing tests which writes on disk

--- tests/base/infrastructure/file_management/file_handler/test_file_handler.py
+++ tests/base/infrastructure/file_management/file_handler/test_file_handler.py
@@ -16,9 +16,9 @@ from base.domain.file_management.file_constants.file_type_values import file_typ
 from base.domain.path_management.path_constants.path_type_values import path_types_values
 
 
-def test_file_handler_validation():
+def test_file_handler_validation_read():
     """
-    test_file_handler_validation
+    test_file_handler_validation_read
     """
 
     root_path = "/home/user1"
@@ -52,71 +52,37 @@ def test_file_handler_validation():
     assert read_data == fake_data
 
 
-def test_file_handler_validation_read():
-    """
-    test_file_handler_validation_read
-    """
-
-    expected_data = '# Test\n\nLet see what happen\ngood luck\ncheers\n\n'
-
-    root_path = Settings.get_root_path().as_posix()
-    target_path = '/config_files/input/configurations/my_read_test.txt'
-
-    path_creator = SimplePathCreator(root_path=root_path)
-    created_path = path_creator.generate_path(target_path=target_path)
-
-    with FileHandler(path_obj=created_path, file_mode=file_mode_values.read) as file_handler:
-        read_data = file_handler.read()
-
-    assert read_data == expected_data
-
-
 def test_file_handler_validation_write():
     """
     test_file_handler_validation_write
     """
 
-    expected_data = '# Test\n\nLet see what happen\ngood luck\ncheers\n\n'
+    root_path = "/home/user1"
+    project_path = "/project1"
+    folder_path = "/folder1"
 
-    root_path = Settings.get_root_path().as_posix()
-    target_path = '/config_files/input/configurations/my_write_test.txt'
+    file_name = "test_file"
+    file_type_suffix = file_type_values.text
 
-    path_creator = SimplePathCreator(root_path=root_path)
-    created_path = path_creator.generate_path(target_path=target_path)
+    target_folder_path = f"{root_path}{project_path}{folder_path}"
+    target_file_path = f"{target_folder_path}/{file_name}{file_type_suffix}"
 
-    with FileHandler(path_obj=created_path, file_mode=file_mode_values.write) as file_handler:
-        file_handler.write(expected_data)
-
-    with FileHandler(path_obj=created_path, file_mode=file_mode_values.read) as file_handler:
-        read_data = file_handler.read()
-
-    assert read_data == expected_data
-
-
-def test_file_handler_validation_create_and_write():
-    """
-    test_file_handler_validation_create_and_write
+    fake_data = """
+    # My Fake Data
+    Data:
+      fake_data
     """
 
-    expected_data = '# Test\n\nLet see what happen\ngood luck\ncheers\n\n'
+    fake_file = FileFaker(file_name=file_name, file_type_suffix=file_type_suffix, initial_content="")
+    fake_file.open()
 
-    root_path = Settings.get_root_path().as_posix()
-    target_path = '/config_files/input/configurations/my_write_test.txt'
+    fake_folder_path = PathFaker(target_path=target_folder_path, target_path_type=path_types_values.directory)
+    fake_file_path = PathFaker(target_path=target_file_path, target_path_type=path_types_values.file, file_obj=fake_file, parent_path=fake_folder_path)
 
     path_creator = SimplePathCreator(root_path=root_path)
-    created_path = path_creator.generate_path(target_path=target_path)
+    created_fake_file_path = path_creator.generate_path(path_obj=fake_file_path)
 
-    with FileHandler(path_obj=created_path, file_mode=file_mode_values.write) as file_handler:
-        file_handler.write(expected_data)
+    with FileHandler(path_obj=created_fake_file_path, file_obj=fake_file, file_mode=file_mode_values.read) as file_handler:
+        file_handler.write(fake_data)
 
-    with FileHandler(path_obj=created_path, file_mode=file_mode_values.read) as file_handler:
-        read_data = file_handler.read()
-
-    assert read_data == expected_data
-
-
-
-
-
-
-
+    assert fake_file.content == fake_data
