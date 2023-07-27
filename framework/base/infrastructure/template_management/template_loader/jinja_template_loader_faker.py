569f7311bb1dc82b0e12b59ca24e7752b98bcd5d
Yair Avalos HobOps Dev <yair.avalos@hobops.io>
2023-07-27 01:58:04 +0000

    fix: removing unnecessary import

--- framework/base/infrastructure/template_management/template_loader/jinja_template_loader_faker.py
+++ framework/base/infrastructure/template_management/template_loader/jinja_template_loader_faker.py
@@ -2,7 +2,6 @@
 
 
 from jinja2 import Environment
-from jinja2 import FileSystemLoader
 
 # Infrastructure
 from framework.base.infrastructure.template_management.template_render import JinjaTemplateRender
